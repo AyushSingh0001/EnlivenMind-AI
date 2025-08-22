58662ec258893599128acae7e0bcd05471060062
Ayush Singh <116361105+AyushSingh0001@users.noreply.github.com>
2025-08-22 23:04:46 +0530

    Delete public directory

--- public/script.js
+++
@@ -1,6 +0,0 @@
-// File: script.js
-const toggle = document.querySelector('.nav-toggle');
-const navList = document.querySelector('.nav-list');
-toggle.addEventListener('click', () => {
-  navList.classList.toggle('show');
-});
